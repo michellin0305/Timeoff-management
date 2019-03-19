4d72de795a1cc1ca82eb08d6fe00eced8445b69b
michellin0305 <mickrixtian@gmail.com>
2019-03-19 19:52:37 +0100

    modified commit

--- src/Components/LoginPage/LoginPage.js
+++ src/Components/LoginPage/LoginPage.js
@@ -8,7 +8,7 @@ class LoginPage extends Component {
         return ( 
             <div>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top navbar-shrink">
-                <a className="navbar-brand" href="/">Navbar</a>
+                <a className="navbar-brand" href="/">TimeOff</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
